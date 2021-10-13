eacfc18a7d8ac3a71bcc493b5ede5e54856fd6df
Jack Kinga <jackmwangi@gmail.com>
2021-10-13 20:45:26 +0300

    add test cmd packages

---
+++ cmd/scanner/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kingzbauer/scraperlang"
+)
+
+func main() {
+	if len(os.Args) <= 1 {
+		fmt.Println("Missing filename")
+		os.Exit(1)
+	}
+	filename := os.Args[1]
+
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
+	}
+
+	scanner := scraperlang.NewScanner(content)
+	tokens, err := scanner.ScanTokens()
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%+v", tokens)
+}
