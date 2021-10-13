868db223c9c1af266c8a885ec270ab134e6b1d84
Jack Kinga <jackmwangi@gmail.com>
2021-10-13 23:54:13 +0300

    update the cfg

--- doc.go
+++ doc.go
@@ -5,7 +5,7 @@ package scraperlang
 	global_defs 		-> tagged_closure ;
 	tagged_closure	-> IDENT "{" expr_statements* "}" ;
 	builtin_funcs		-> getExpr | printExpr ;
-	expr_statements	-> expressions | builtin_funcs ;
+	expr_statements	-> ( expressions | builtin_funcs ) terminator ;
 	expression 			-> assignExpr |
 										 callExpr |
 										 closureExpr |
@@ -13,23 +13,27 @@ package scraperlang
 										 htmlAttrAccessor |
 										 arrayExpr |
 										 mapExpr |
+										 mapAccess |
+										 arrayAccess |
 										 primary ;
 	getExpr					-> tag? "get" expression ("," expression) ;
 	tag							-> "@"IDENT ;
 	assignExpr 			-> IDENT "=" ( primary | expression ) ;
 	printExpr				-> expression ( "," expression )* ;
 	callExpr				-> IDENT expression ( "," expression ) ;
-	closureExpr			-> "{" params? expr_statements* "}" ( NEWLINE | EOF ) ;
+	closureExpr			-> "{" params? expr_statements* "}" ;
 	accessExpr 			-> IDENT "." IDENT ;
 	htmlAttrAccessor-> IDENT "~" IDENT ;
-	arrayExpr				-> "[" NEWLINE* arrayEntry NEWLINE* ( "," NEWLINE* arrayEntry NEWLINE* )* "]" ( NEWLINE | EOF ) ;
+	arrayExpr				-> "[" NEWLINE* arrayEntry NEWLINE* ( "," NEWLINE* arrayEntry NEWLINE* )* "]" ;
 	arrayEntry			-> ( primary
 										 | callExpr
 										 | arrayExpr
 										 | mapExpr
 										 | htmlAttrAccessor
 										 | accessExpr ) ;
-	mapExpr					-> "{" NEWLINE* mapEntry NEWLINE* ( "," NEWLINE* mapEntry NEWLINE* )* "}" ( NEWLINE | EOF ) ;
+	mapExpr					-> "{" NEWLINE* mapEntry NEWLINE* ( "," NEWLINE* mapEntry NEWLINE* )* "}" ;
 	mapEntry				-> STRING ":" arrayEntry ;
+	mapAccess				-> IDENT "[" STRING "]" ;
 	primary					-> STRING | NUMBER | TRUE | FALSE | NIL | IDENT ;
+	terminator			-> NEWLINE+ ;
 */
